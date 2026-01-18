bac6aa443ff7295156aa9ffab6f9a4e38099db75
dennie <denitsa.sureva@icloud.com>
2026-01-18 12:31:59 +0100

    feat: comp Header and styles

---
+++ components/header/Header.jsx
@@ -0,0 +1,31 @@
+import { Text, TouchableOpacity, View } from 'react-native';
+import { styles } from './styles';
+import { useState } from 'react';
+
+
+export default function Header() {
+    const [showAddExpenseModal, setShowAddExpenseModal] = useState(false);
+
+    const addExpensePressHandler = () => {
+        setShowAddExpenseModal(true)
+    }
+
+
+    return (
+        <View style={styles.section}>
+            <View >
+                <Text style={styles.heading}>Hello! 👋 </Text>
+                <Text style={styles.text}>Track your expenses wisely!</Text>
+            </View>
+            <View>
+                <TouchableOpacity onPress={addExpensePressHandler}>
+                    <View style={{ backgroundColor: '#4F39F6', padding: 5, borderRadius: 10 }}>
+                        <PlusIcon size={32} color={'white'} />
+                    </View>
+                </TouchableOpacity>
+
+            </View>
+
+        </View>
+    );
+};
---
+++ styles.js
@@ -0,0 +1,25 @@
+import { StyleSheet } from "react-native";
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+      
+    },
+    section: {
+        paddingTop: 70,
+        flexDirection: 'row',
+        padding:30,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    heading: {
+        fontSize:28,
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
+    text: {
+        fontSize:18,
+    },
+});
